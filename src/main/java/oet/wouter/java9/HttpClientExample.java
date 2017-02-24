daf8ed1475013b1db05aa383c4e78a9c5aff6b96
Wouter Oet <wouter.oet@gmail.com>
2017-02-24 07:11:09 +0100

    Disabled the httpclient

--- src/main/java/oet/wouter/java9/HttpClientExample.java
+++ src/main/java/oet/wouter/java9/HttpClientExample.java
@@ -1,18 +1,19 @@
 package oet.wouter.java9;
 
 import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpResponse;
+//import java.net.http.HttpClient;
+//import java.net.http.HttpResponse;
 
 public class HttpClientExample {
 
+    // TODO the HttpClient changed its interface and package, update this
     public static void main(String[] args) throws Exception{
-        HttpClient.getDefault()
-                .request(URI.create("http://www.quintor.nl"))
-                .GET()
-                .responseAsync() // CompletableFuture :D
-                .thenAccept(httpResponse -> System.out.println(httpResponse.body(HttpResponse.asString())));
-
-        Thread.sleep(5000); // The response is handled in a different thread. Give it some time to finish.
+//        HttpClient.getDefault()
+//                .request(URI.create("http://www.quintor.nl"))
+//                .GET()
+//                .responseAsync() // CompletableFuture :D
+//                .thenAccept(httpResponse -> System.out.println(httpResponse.body(HttpResponse.asString())));
+//
+//        Thread.sleep(5000); // The response is handled in a different thread. Give it some time to finish.
     }
 }
